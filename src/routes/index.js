5042f2fc17745342528fbdd2144f4c6c30d61e0b
Ivan Guerrero [GDL] <iguerrero@compusoluciones.com>
2023-03-28 16:38:37 -0600

    nueva arquitectura

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,8 +7,6 @@ import pagesRouter from './pages/index.js';
 import productsRoutes from './products/index.js'
 import messagesRoutes from './messages/index.js'
 
-import twitterRoutes from './twitterAuth/index.js'
-
 import infoRoutes from './info/index.js'
 import randomRoutes from './random/index.js'
 
@@ -17,10 +15,13 @@ const router = express.Router();
 
 router.use(pagesRouter)
 router.use('/api', authRoutes);
-router.use('/api',twitterRoutes)
 router.use('/api', productsRoutes)
 router.use('/api', messagesRoutes);
 router.use('/api', infoRoutes)
 router.use('/api', randomRoutes)
+
+router.get('/api/auth/twitter', passport.authenticate('twitter'));
+router.get('/api/auth/twitter/callback', passport.authenticate('twitter', {successRedirect: '/', failureRedirect: '/signin'}));
+
 
 export default router;
